5718db889535d1dd24df1d430117125d4e98a8ce
Volodymyr Babenko <gomerbabenko@gmail.com>
2024-05-24 10:12:49 +0300

    feat: add 04 module

---
+++ APIs/04/index.js
@@ -0,0 +1,155 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+const taskSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: true }
+})
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  log: [taskSchema]
+});
+const User = mongoose.model("User", userSchema);
+
+const findUsers = async (res) => {
+  const users = await User.find({}).select({"username": 1, });
+  // console.log(users)
+  res.json(users)  
+};
+
+
+app.get("/api/demo", (req, res) => {
+  findUsers(res)
+
+  // res.json({
+  //   demo: "demo"
+  // })
+})
+
+app.post("/api/users", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    count: 1,
+    log: []
+  })
+  user.save()
+  
+  res.json({
+      "username": user.username,
+      "_id": user._id
+  })
+})
+
+app.get("/api/users", (req, res) => {
+  findUsers(res)
+  // [{"_id":"61204ee9f5860e05a3652f11","username":"fcc_test_16295073016","__v":0},{"_id":"61206b40f5860e05a3652f21","username":"tejpaulsingh","__v":0},{"_id":"6120737df5860e05a3652f23","username":"hitesh","__v":0},{"_id":"6120ae24f5860e05a3652f32","username":"asd","__v":0}]
+})
+
+const addTask = async (res, id, description, duration, date) => {
+  try {
+    // TODO: continue
+    const user = await User.findOne({_id: id});
+    user.log.push({
+      description: description,
+      duration: +duration,
+      date: date
+    })
+    // console.log(user)
+    user.save()
+
+    res.json({
+      _id: id,
+      username: user.username,
+      date: date.toDateString(),
+      duration: +duration,
+      description: description
+    })
+  } catch (e) {
+    res.json({
+      error: e
+    })
+  }
+};
+
+
+app.post("/api/users/:id/exercises", (req, res) => {
+  const id = req.params.id
+  const {description, duration, date} = req.body
+  // console.log("date", date)
+  addTask(res, id, description, duration, new Date(date))
+  
+})
+
+const aboutUser = (user, query) => {
+  const log = []
+  let count = 0
+  for (const elem of user.log) {
+    // console.log("* from", query.from, "elem.data", elem.date, query.from <= elem.date)
+    if (query.from !== undefined && query.from > elem.date) {
+      continue
+    }
+    if (query.to !== undefined && query.to < elem.date) {
+      continue
+    }
+
+    log.push({
+      description: elem.description,
+      duration: elem.duration,
+      date: elem.date.toDateString(),
+    })
+    count++
+    if (query.limit === count) {
+      break      
+    }
+    // console.log("elem", elem)
+  }
+  return {
+      _id: user.id,
+      username: user.username,
+      count: count,
+      log: log
+  }
+}
+
+const findUser = async (res, id, query) => {
+  const user = await User.findOne({_id: id})
+  // console.log(user)
+  res.json(aboutUser(user, query))
+};
+
+app.get("/api/users/:id/logs", (req, res) => {
+  // console.log(req.query)
+  const query = {}
+  for (key of ["from", "to"]) {
+    const value = req.query[key]
+    if (value) {
+      query[key] = new Date(value)
+    }
+  } 
+  if (req.query["limit"]) {
+    query["limit"] = +req.query["limit"]
+  }
+  // console.log("query", query)
+  findUser(res, req.params.id, query)
+})
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
